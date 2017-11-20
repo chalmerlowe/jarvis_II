21d0ab7340989bbd0d6837d362206c7e3ecf5425
Chalmer Lowe <chalmer.lowe@gmail.com>
2017-11-19 16:56:49 -1000

    added sample puzzle AND revisions to stats lesson

---
+++ sample_puzzles/10_closest_match/empty_closest_match.py
@@ -0,0 +1,29 @@
+# TITLE: closest_matches:
+# AUTHOR: Chalmer Lowe
+# DESCRIPTION:
+
+# Given the word list (words.txt) associated with this
+#     puzzle** and the target word: 'octopus'
+#     0) identify the 10 words "closest" to the target word
+#     1) count the total number of characters in the 10
+#        words
+#
+# NOTE: The Python Standard Library provides a specific
+#     tool to identify close matches between strings.
+#     (There is NO need to install 3rd party libraries
+#      such as NLTK.)
+
+# For example, given this list and the target word 'help':
+# ['hello', 'help', 'hide', 'hem', 'heft', 'them', 'heel']
+
+# The following words are the three words most closely
+#     matched sequentially to the target word 'help'.
+
+# * help
+# * hell
+# * hello
+
+# **NOTE: The word list was derived from
+#         /usr/share/dict/words on a Mac
+
+# Put your code here:
