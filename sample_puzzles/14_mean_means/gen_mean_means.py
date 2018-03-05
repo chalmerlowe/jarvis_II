891de7ff9907bc37bcd86bbb260d5bff4b94366b
Chalmer Lowe <chalmer.lowe@gmail.com>
2018-03-05 05:42:04 -1000

    finished puzzle 14 mean means

---
+++ sample_puzzles/14_mean_means/gen_mean_means.py
@@ -0,0 +1,43 @@
+# TITLE: mean means generator >> gen_mean_means.py
+# AUTHOR: Chalmer Lowe
+# DESCRIPTION:
+
+# The file: mean_means.csv contains line upon line of numbers.
+#     Each line has a random number of numbers, separated by commas.
+#     Read each line and calculate the mean (average) for that line, rounded to
+#     three digits.
+#     Find all the means between 81 and 92, exclusive (81 < m < 92) and
+#     calculate the mean of just those values, again rounding the final
+#     result to three digits.
+#
+#     NOTE: to calculate the mean, sum a series of numbers and divide by
+#           the total number of numbers.
+
+# For example, if you were given the constraint that you are looking for means
+#     between 81 and 92:
+#     98, 99, 97, 92, 91          > average = 95.4       > out of bounds
+#     82, 83, 90, 92, 91          > average = 87.6       > in of bounds
+#     82.5, 83, 90.3456, 92, 91   > average = 87.769     > in of bounds, rounded
+#     71, 78, 80, 83, 91          > average = 80.6       > out of bounds
+
+#     87.6, 87.769      > answer = 87.684
+
+#
+# ----------------------------------
+# 14) Mean Means
+
+from random import random, randint, gauss
+linecount = 1000
+
+with open('mean_means.csv', 'w') as fout:
+
+    for line in range(linecount):
+        nums = []
+        mu = randint(76, 127)
+        sigma = randint(5, 11)
+        for num in range(randint(10,101)):
+            nums.append(str(gauss(mu, sigma)))
+        output = ','.join(nums) + '\n'
+        fout.write(output)
+
+fout.close()
---
+++ sample_puzzles/14_mean_means/your_code_here_mean_means.py
@@ -0,0 +1,27 @@
+# TITLE: mean means empty >> your_code_here_mean_means.py
+# AUTHOR: Chalmer Lowe
+# DESCRIPTION:
+
+# The file: mean_means.csv contains line upon line of numbers.
+#     Each line has a random number of numbers, separated by commas.
+#     Read each line and calculate the mean (average) for that line, rounded to
+#     three digits.
+#     Find all the means between 81 and 92, exclusive (81 < m < 92) and
+#     calculate the mean of just those values, again rounding the final
+#     result to three digits.
+#
+#     NOTE: to calculate the mean, sum a series of numbers and divide by
+#           the total number of numbers.
+
+# For example, if you were given the constraint that you are looking for means
+#     between 81 and 92:
+#     98, 99, 97, 92, 91          > average = 95.4       > out of bounds
+#     82, 83, 90, 92, 91          > average = 87.6       > in of bounds
+#     82.5, 83, 90.3456, 92, 91   > average = 87.769     > in of bounds, rounded
+#     71, 78, 80, 83, 91          > average = 80.6       > out of bounds
+
+#     87.6, 87.769      > answer = 87.684
+
+#
+# ----------------------------------
+# Your code here:
