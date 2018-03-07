32eaece3484e43de0b09dafc29f161875466ec4e
John Johnson <john@onebreathphoto.com>
2018-03-07 12:10:56 -1000

    John Johnson's 0.5 Donkey solution

--- sample_puzzles/19_datetimes/john_soln_datetimes.py
+++ sample_puzzles/19_datetimes/john_soln_datetimes.py
@@ -1,8 +1,21 @@
-import datetime
+from dateutil import parser
+from datetime import datetime,timedelta
 
+tsset = set()
+tslist = []
 lastts = ''
-with file('timestamps.txt') as f:
-	ts = f.readline()
-	
+maxdelta=0
+with open('timestamps.txt') as f:
+	tslist = f.read().split('\n')
+for ts in tslist:
+	if ts!='':
+		tsset.add( parser.parse( ts ) )
+tslist = sorted( tsset )
+for ts in tslist:
+	if lastts!='':
+		if maxdelta<(ts - lastts).total_seconds():
+			maxdelta = (ts - lastts).total_seconds()
+	lastts = ts
+print('Biggest difference between timestamps is '+str(maxdelta)+' seconds.')
 	
 	
