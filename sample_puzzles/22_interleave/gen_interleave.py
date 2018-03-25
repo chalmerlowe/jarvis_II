e1cb61cc82dafae508251f2356add887d9ccefb4
Chalmer Lowe <chalmer.lowe@gmail.com>
2018-03-24 19:02:43 -1000

    added interleave puzzle

---
+++ sample_puzzles/22_interleave/gen_interleave.py
@@ -0,0 +1,46 @@
+# TITLE: interleave >> gen_interleave.py
+# AUTHOR: Chalmer Lowe
+# DESCRIPTION:
+
+# This code generates the input file for the interleave puzzle.
+
+# You are given a file (interleave.txt) with two lines.
+
+# Each line contains numbers separated by semicolons.
+# You will need to interleave the values from the two lines and calculate
+#     the difference between each sequential pair of numbers.
+# Sum all the even differences.
+
+# For example... given the following numbers...
+
+#     a = [1, 4, 9]
+#     b = [8, 3, 3]
+
+# interleaved_nums = [1,  8,  4,  3,  9,  3]
+#                     ^   ^   ^   ^   ^   ^
+#                    a1, b1, a2, b2, a3, b3
+
+# If we examine, sequentially, all pairs of values to find any pairs that have
+#     a difference between them that is even:
+
+# between a1 and b1: 1 - 8 = -7   which is an odd difference
+# between b1 and a2: 8 - 4 = 4    which is an even difference
+# between a2 and b2: 4 - 3 = 1    which is odd
+# between b2 and a3: 3 - 9 = -6   which is even
+# between a3 and b3: 9 - 3 = 6    which is even
+
+# Adding all the even differences yields:
+#     4 + -6 + 6 = 4
+
+# Answer = 4
+
+# ==============================================================
+# Your code goes here:
+
+from random import randint
+
+with open('interleave.txt', 'w') as fout:
+    for line in range(2):
+        nums = [str(randint(1, 43)) for num in range(100)]
+        nums = ';'.join(nums) + '\n'
+        fout.write(nums)
