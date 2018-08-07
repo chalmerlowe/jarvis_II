98baea15de6f5798692adf22e69db47e283defd1
Chalmer Lowe <chalmer.lowe@gmail.com>
2018-08-07 09:30:37 -0700

    update to part II solution and instructions

--- sample_puzzles/30_rsa_token_pattern/gen_rsa_part_II.py
+++ sample_puzzles/30_rsa_token_pattern/gen_rsa_part_II.py
@@ -1,17 +1,17 @@
 # This problem is based on the same scenario as our previous problem
 # but the data is stored in a json file: rsa_token_values_II.json 
 
-# As before, there are five main categories associated with 
+# As before, there are several main categories associated with 
 #     patterns potentially found in the tokens:
 
-#     * 123 321     - category 1: a palindrome if you consider all six numbers
-#     * 121 454     - category 2: two mini palindromes, one in each group of three
+#     * 123 321     - CATEGORY 1: a palindrome if you consider all six numbers
+#     * 121 454     - CATEGORY 2: two mini palindromes, one in each group of three
 #                     (they do not need to be identical)
-#     * 123 679     - category 3: rising numbers (each subsequent number is equal to 
+#     * 123 669     - CATEGORY 3: rising numbers (each subsequent number is equal to 
 #                     or larger than the previous number)
-#     * 654 321     - category 4: falling numbers (each subsequent number is
+#     * 654 421     - CATEGORY 4: falling numbers (each subsequent number is
 #                     equal to or smaller than the previous number)    
-#     * 235 156     - category 5: none of the above
+#     * 235 156     - CATEGORY 5: none of the above
 
 # The json file contains a sequence of records
 #     with four fields per record:
@@ -20,15 +20,27 @@
 #     * score: confidence score (0 (zero confidence) to 10 (high confidence))
 #     * token: the RSA token value
 # 
-#     * The data will look like this:
+#     * An example of the data looks like this:
 #     [{uid: 0000, cat: 1, score: 4, token: '123 321'},
-#      {uid: 0001, cat: 2, score: 5, token: '121 434'}]
+#      {uid: 0015, cat: 4, score: 5, token: '121 454'},
+#      {uid: 0015, cat: 3, score: 7, token: '123 669'},
+#      {uid: 0010, cat: 4, score: 3, token: '654 421'},
+#      {uid: 0010, cat: 5, score: 9, token: '235 156'},
+#      etc...]
+#
+#      Notice, that in the above sample, the first cat value (1) matches
+#      the token category of type 1, but the second cat value (4) does 
+#      note match the token category of type 2.
+
 
 # The objective is to read in the json and answer the following questions:
 #     * for each of the five token categories described above, how many
 #           were found?
 #     * in how many cases does the given token category match
 #           the actual token category?
+
+# For our example data, each of the five token categories is found once 
+#     but only four of the actual categories match the given cat value.
 
 from random import randint, choice, sample 
 import json
