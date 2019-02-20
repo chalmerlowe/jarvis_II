308d2a8f6a6ffe1e0906651ebd7ab30369c35837
Chalmer Lowe <chalmer.lowe@gmail.com>
2019-02-19 21:34:14 -1000

    added solution to my_basics exercise

---
+++ 01_basics/soln_my_basics.py
@@ -0,0 +1,27 @@
+# Dark Lord of Python, 20190219
+
+print('Dark Lord of Python')
+
+print()
+
+myinteger = int('13')
+
+print(myinteger)
+
+fname = input('What is your name? ')
+
+length = len(fname)
+
+print(length)
+
+neg_number = str(-42)
+
+int_number = int(81)
+
+print(neg_number)
+
+print(int_number)
+
+comparison = 'selina kyle' == 'harleen quinzel'
+
+print(comparison)
